d17e90d4b02942100762e1912c65a293cf0f92ab
fitdizzle <mark@idevforweb.com>
2023-01-17 06:47:20 -0700

    added dashboard controllers and views. Modified navigation to render conditionally for dashboard and page views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get '/dashboard' => 'dashboard#index', as: :dashboard
+  get 'dashboard/properties'
+  get 'dashboard/reports'
+  resources :properties
+  devise_for :accounts
+  devise_for :models
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'public#main'
 end
